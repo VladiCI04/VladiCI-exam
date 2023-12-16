186e93a64b644db7839385a9601d1933bf4ecef7
VladiCI04 <vladi.c.i@abv.bg>
2023-12-16 14:18:27 +0200

    Fix UI Tests

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -2,8 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
     await page.goto('http://localhost:8080'); 
-    const homeLink = await page.$('p[href="/"]');
+    const homeLink = await page.$('a[href="/"]');
     const text = await homeLink.textContent();
-    expect(text).toBe('My Shopping List for Tomorrow');
+    expect(text).toBe('Home');
   });
   
--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-list');
-    const list = await page.$('dl');
+    await page.goto('http://localhost:8080/Shopping-List');
+    const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
   
